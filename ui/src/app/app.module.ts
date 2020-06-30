8f711e81a2aa948544fde93ef0e14174a143577b
Christopher Mogler <mogler@weissedv.de>
2020-06-30 11:32:40 +0200

    Todoliste ruft über API Helper Daten ab

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -6,22 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 import { TodoListComponent } from './todoList/todoList.component';
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      TodoListComponent
-   ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule,
-      BrowserAnimationsModule,
-      MatToolbarModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, TodoListComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- ui/src/app/todoList/todoList.component.ts
+++ ui/src/app/todoList/todoList.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { API_Todos } from '../../api';
 @Component({
   selector: 'app-todoList',
   templateUrl: './todoList.component.html',
-  styleUrls: ['./todoList.component.css']
+  styleUrls: ['./todoList.component.css'],
 })
 export class TodoListComponent implements OnInit {
-  todos = []
+  todos = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    API_Todos.getAllTodos()
+      .then((res) => res.json())
+      .then((res) => {
+        this.todos = res;
+      });
   }
-
 }
